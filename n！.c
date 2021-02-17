5d032b182f348aa421bc0b47118a850244e880ef
panghutx <bigzhi666@126.com>
2021-02-17 17:31:00 +0800

    2/17

---
+++ n！.c
@@ -0,0 +1,15 @@
+﻿//计算n！
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int n = 0;
+	int result = 1;
+	scanf("%d", &n);
+	for (a = 1; a <= n; a++)
+	{
+		result = result*a;
+	}
+	printf("%d", result);
+	return 0;
+}
