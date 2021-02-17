79e98898a9fd315649ae675b8d588e14fb7c554e
panghutx <bigzhi666@126.com>
2021-02-17 23:17:11 +0800

    选择和循环

---
+++ 选择和循环/for循环.c
@@ -0,0 +1,26 @@
+﻿//num 1
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+			printf("1");//输出100个1
+	}
+	return 0;
+}
+//num 2
+#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	for (; a < 10; a++)
+	{
+		for (; b < 10; b++)
+			printf("1");//输出10个1
+	}
+	return 0;
+}
