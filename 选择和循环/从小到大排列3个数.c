5635df83fb6af6a1245e4dbb962e720ee82ac24a
panghutx <bigzhi666@126.com>
2021-02-19 21:22:37 +0800

    2/19

---
+++ 选择和循环/从小到大排列3个数.c
@@ -0,0 +1,31 @@
+﻿#include<stdio.h>
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	scanf("%d%d%d", &a, &b, &c);
+	if (a < b)
+	{
+		int d;
+		d = a;
+		a = b;
+		b = d;
+	}
+	if (a < c)
+	{
+		int d;
+		d = a;
+		a = c;
+		c = d;
+	}
+	if (b<c)
+	{
+		int d;
+		d = b;
+		b = c;
+		c = d;
+	}
+	printf("%d%d%d", a, b, c);
+	return 0;
+}
