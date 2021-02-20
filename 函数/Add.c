08d830a817e1ff3a999d2e8885854c4d710eae5b
panghutx <bigzhi666@126.com>
2021-02-20 22:29:31 +0800

    2/20

---
+++ 函数/Add.c
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+//自定义函数
+Add(int x, int y)
+{
+	int z = x + y;
+	return z;
+}
+int main()
+{
+	int a = 0;
+	int b = 0;
+	int add = Add(3, 4);
+	printf("%d", add);
+	return 0;
+}
---
+++ 函数/memset.c
@@ -0,0 +1,11 @@
+﻿/* memset example */
+#include <stdio.h>
+#include <string.h>
+
+int main ()
+{
+  char str[] = "almost every programmer should know memset!";
+  memset (str,'-',6);
+  printf("%s",str);
+  return 0;
+}
