288873813c4abd1814aa2680f89e57200604c2fc
panghutx <bigzhi666@126.com>
2021-02-22 22:58:33 +0800

    2/22

---
+++ 函数/Swap.c
@@ -0,0 +1,17 @@
+﻿#include<stdio.h>
+void Swap(int* pa, int* pb)
+{
+	int temp = 0;
+	temp = *pa;
+	*pa = *pb;
+	*pb = temp;
+	
+}
+int main()
+{
+	int x=10;
+	int y=20;
+	Swap(&x, &y);
+	printf("x=%d,y=%d", x, y);
+	return 0;
+}
